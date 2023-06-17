b24bdf852e0e9433d4de1029743402c315331bb6
FADHLILLAH <73236007+fadhlillah2@users.noreply.github.com>
2023-06-18 06:11:06 +0700

    logging setup

--- src/main/java/com/kipas/miniprojectrestapi/services/CustomerService.java
+++ src/main/java/com/kipas/miniprojectrestapi/services/CustomerService.java
@@ -3,6 +3,8 @@ package com.kipas.miniprojectrestapi.services;
 import com.kipas.miniprojectrestapi.entities.Customer;
 import com.kipas.miniprojectrestapi.exceptions.ResourceNotFoundException;
 import com.kipas.miniprojectrestapi.repositories.CustomerRepository;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -14,7 +16,10 @@ public class CustomerService {
     @Autowired
     private CustomerRepository customerRepository;
 
+    private static final Logger log = LoggerFactory.getLogger(OrderService.class);
+
     public Customer saveCustomer(Customer customer) {
+        log.info("Customer successfully processed: {}", customer);
         return customerRepository.save(customer);
     }
 
--- src/main/java/com/kipas/miniprojectrestapi/services/ProductService.java
+++ src/main/java/com/kipas/miniprojectrestapi/services/ProductService.java
@@ -3,6 +3,8 @@ package com.kipas.miniprojectrestapi.services;
 import com.kipas.miniprojectrestapi.entities.Product;
 import com.kipas.miniprojectrestapi.exceptions.ResourceNotFoundException;
 import com.kipas.miniprojectrestapi.repositories.ProductRepository;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -14,7 +16,10 @@ public class ProductService {
     @Autowired
     private ProductRepository productRepository;
 
+    private static final Logger log = LoggerFactory.getLogger(OrderService.class);
+
     public Product saveProduct(Product product) {
+        log.info("Product successfully processed: {}", product);
         return productRepository.save(product);
     }
 
